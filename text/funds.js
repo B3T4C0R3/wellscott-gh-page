d43a5be054189a50940bbdc57e187e0478c51a0d
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2024-08-29 21:18:59 +0000

    deploy: B3T4C0R3/wellscott@e8fc2cc1bfa110aac5bb34cabb51b19270e19289

--- text/funds.js
+++ text/funds.js
@@ -1,7 +1,7 @@
 const funds = {
   pandion: {
-    name: 'Sustainable Equity Flagship Fund',
-    logo: '/images/logos/pandion.png',
+    name: 'Global Sustainable Equity Long Short Fund',
+    logo: '/images/logos/pandion.jpg',
     strategy: 'Sustainable equities portfolio',
     liquidity: 'Monthly',
     structure: 'Open end',
@@ -17,9 +17,27 @@ const funds = {
     text:
       'The objective is to achieve long term capital growth. To achieve this, the strategy is to invest in predominantly a diversified portfolio of Investments Funds. The portfolio is composed of Investments Funds with various strategies. The portfolio will be managed without a benchmark and specified return objectives.'
   },
-  abc: {
-    name: 'Alpha Based Capital',
-    logo: '/images/logos/abc.png',
+  flagship: {
+    name: 'DB Flagship Fund',
+    logo: '/images/logos/flagship.png',
+    strategy: 'Automated trading strategies',
+    liquidity: 'Monthly',
+    structure: 'Open end',
+    text:
+      'The objective is to achieve long term capital growth. To achieve this, the strategy is to invest in predominantly a diversified portfolio of Investments Funds. The portfolio is composed of Investments Funds with various strategies. The portfolio will be managed without a benchmark and specified return objectives.'
+  },
+  brandcapital: {
+    name: 'Brand Capital Fund',
+    logo: '/images/logos/brandcapital.png',
+    strategy: 'Automated trading strategies',
+    liquidity: 'Monthly',
+    structure: 'Open end',
+    text:
+      'The objective is to achieve long term capital growth. To achieve this, the strategy is to invest in predominantly a diversified portfolio of Investments Funds. The portfolio is composed of Investments Funds with various strategies. The portfolio will be managed without a benchmark and specified return objectives.'
+  },
+  goref: {
+    name: 'GoRef Fund',
+    logo: '/images/logos/goref.jpg',
     strategy: 'Automated trading strategies',
     liquidity: 'Monthly',
     structure: 'Open end',
